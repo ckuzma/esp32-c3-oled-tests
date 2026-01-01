b588ec9d565ade75022bdb192713f3903b131e2d
Chris Kuzma <kuzma@Gelb.local>
2025-12-31 18:28:18 -0800

    Added stack dump from crash

--- ntp-display/ntp-display.ino
+++ ntp-display/ntp-display.ino
@@ -1,5 +1,13 @@
+// Libraries to get time from NTP Server
+#include <WiFi.h>
+#include <time.h>
+// Display library
 #include <U8g2lib.h>
-// Library imports
+
+// Replace with your network credentials
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+
 #define OLED_RESET U8X8_PIN_NONE
 #define OLED_SDA 5
 #define OLED_SCL 6
@@ -16,8 +24,6 @@ int yOffset = 0;                // Set to 0 if flipDisplaySetting == 1, otherwis
 int xOffset = 28;               // Seems to be good at 28 regardless of the display orientation
 int counter = 0;                // Used to display runtime in seconds
 
-// Program variables
-bool blinkDisplayBool = false;  // Simple 
 
 void display_content(int counter) {
   u8g2.clearBuffer();
@@ -27,22 +33,6 @@ void display_content(int counter) {
   char buffer[21];
   snprintf(buffer, sizeof(buffer), " %d", counter);
   u8g2.drawStr(xOffset + 0, yOffset + 24, buffer);
-
-  // Blink the corner pixels
-  if(blinkDisplayBool == false) {
-    // Blink Rectangle
-    // u8g2.drawBox(xOffset, yOffset, 72, 40); // This is the biggest rectangle
-
-    // Blink corner dots
-    u8g2.drawBox(xOffset, yOffset, 1, 1); // Top left
-    u8g2.drawBox(xOffset+71, yOffset, 1, 1); // Top right
-    u8g2.drawBox(xOffset, yOffset+39, 1, 1); // Bottom left
-    u8g2.drawBox(xOffset+71, yOffset+39, 1, 1); // Bottom right
-    blinkDisplayBool = true;
-  }
-  else {
-    blinkDisplayBool = false;
-  }
   
   // Send it
   u8g2.sendBuffer();
@@ -50,17 +40,30 @@ void display_content(int counter) {
 
 
 void setup(void) {
-  Serial.begin(115200);
-  Serial.println("Ready to start!");
   u8g2.begin();
   u8g2.setContrast(255);                // Set contrast to maximum because it's an OLED with only on/off
   u8g2.setBusClock(400000);             // 400kHz I2C
   u8g2.setFlipMode(flipDisplaySetting); // Set which orientation is up
+
+  // Connect to WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  // Print the status
+  char buffer[30];
+  snprintf(buffer, sizeof(buffer), " %s", "Connecting to WiFi ..");
+  u8g2.drawStr(xOffset + 0, yOffset + 10, buffer);
+
+  // Check for non-connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+
+  // Print the status
+  snprintf(buffer, sizeof(buffer), " %s", "Connecting to WiFi ..");
+  u8g2.drawStr(xOffset + 0, yOffset + 24, buffer);
 }
 
 void loop(void) {
-  display_content(counter);
-  Serial.println(counter);
-  counter++;                            // Iterate the counter
-  delay(1000);
+
 }
